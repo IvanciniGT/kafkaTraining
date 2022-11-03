161fa9d8f66c21392210a5ef2cd0bd3bdb628b8e
Ubuntu <ubuntu@ip-172-31-3-144.eu-west-1.compute.internal>
2022-11-03 19:03:15 +0000

    day 9

---
+++ installations/confluent/prepareFolders.sh
@@ -0,0 +1,11 @@
+mkdir -p /home/ubuntu/environment/data/confluent/zookeeper1-data
+mkdir -p /home/ubuntu/environment/data/confluent/zookeeper1-logs
+mkdir -p /home/ubuntu/environment/data/confluent/zookeeper2-data
+mkdir -p /home/ubuntu/environment/data/confluent/zookeeper2-logs
+mkdir -p /home/ubuntu/environment/data/confluent/zookeeper3-data
+mkdir -p /home/ubuntu/environment/data/confluent/zookeeper3-logs
+mkdir -p /home/ubuntu/environment/data/confluent/kafka1
+mkdir -p /home/ubuntu/environment/data/confluent/kafka2
+mkdir -p /home/ubuntu/environment/data/confluent/kafka3
+
+chmod -R 777 /home/ubuntu/environment/data/confluent
